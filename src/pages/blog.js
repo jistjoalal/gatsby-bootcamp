c28d1344303bbef1476ff61864c5d8e7397de234
jistjoalal <jistjoalal@gmail.com>
2019-05-07 17:03:33 -0700

    footer + blog page styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,19 +3,21 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
+import blogStyles from "./blog.module.scss"
+
 export default () => (
   <Layout>
     <h1>blog page</h1>
-    <ol>{allMarkdownPosts().map(PostTitle)}</ol>
+    <ol className={blogStyles.posts}>{allMarkdownPosts().map(PostTitle)}</ol>
   </Layout>
 )
 
 const PostTitle = ({ node }) => (
-  <li key={node.id}>
+  <li key={node.id} className={blogStyles.post}>
     <Link to={`/blog/${node.fields.slug}`}>
       <h2>{node.frontmatter.title}</h2>
+      <p>{node.frontmatter.date}</p>
     </Link>
-    <p>{node.frontmatter.date}</p>
   </li>
 )
 
