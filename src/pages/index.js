bb2a091ab29a33f337a33d25f6673f7c0af92bf6
jistjoalal <jistjoalal@gmail.com>
2019-05-09 13:19:18 -0700

    remove contact link from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
@@ -9,8 +8,5 @@ export default () => (
     <Head title="Home" />
     <h1>hello.</h1>
     <h2>this is a blog written by a person</h2>
-    <p>
-      Need a developer? <Link to="/contact">Contact me</Link>
-    </p>
   </Layout>
 )
